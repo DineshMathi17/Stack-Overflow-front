d63cd8534936dac9784e13781abefaa513137038
DineshMathi17 <dineshbabum17d@gmail.com>
2023-07-29 10:27:28 +0530

    save all

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { useSelector,useDispatch } from "react-redux"
-import Avatar from '../../Components/Avatar/Avatar'
+import Avatar from '../Avatar/Avatar'
 import { setCurrentUser } from "../../actions/currentUser"
 import decode from "jwt-decode"
 
